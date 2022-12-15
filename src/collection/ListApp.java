bad846dcba371a7f3269da343b27ce3d9c371254
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-12-15 15:48:55 +0700

    list

---
+++ src/collection/ListApp.java
@@ -0,0 +1,20 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListApp {
+    public static void main(String[] args) {
+        // ArrayList<String> names = new ArrayList<>();
+        List<String> names = new LinkedList<>();
+
+        names.add("Rizal");
+        names.add("Fadia");
+        names.add("Al");
+        names.add("Fikri");
+
+        System.out.println(names.get(0));
+        System.out.println(names.get(2));
+    }
+}
