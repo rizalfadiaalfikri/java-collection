d0034d9d0c011bfbb7d59e4af08353ba732e5ab7
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-12-17 20:57:17 +0700

    collections class

---
+++ src/collection/CollectionsApp.java
@@ -0,0 +1,20 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsApp {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.addAll(List.of("Rizal", "Fadia", "Al", "Fikri"));
+
+        System.out.println(list);
+
+        Collections.reverse(list);
+        System.out.println(list);
+
+        Collections.shuffle(list);
+        System.out.println(list);
+    }
+}
